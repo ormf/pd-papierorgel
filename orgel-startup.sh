cbbaedba52e5d178d932ae36e3de183a31cb8700
ormf <junk14@selma.hfmdk-frankfurt.de>
2023-08-08 17:31:01 +0200

    updated for web-gui handling.

--- orgel-startup.sh
+++ orgel-startup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 pd -rt -jack -channels 1 -send "id orgel01" orgelmodul-dsp-osc.pd &
-# pd -rt -jack -channels 1 -nogui -send "id orgel02" orgelmodul-dsp-osc.pd &
-# pd -rt -jack -channels 1 -nogui -send "id orgel03" orgelmodul-dsp-osc.pd &
-# pd -rt -jack -channels 1 -nogui -send "id orgel04" orgelmodul-dsp-osc.pd &
-# pd -rt -jack -channels 1 -nogui -send "id orgel05" orgelmodul-dsp-osc.pd &
-# pd -rt -jack -channels 1 -nogui -send "id orgel06" orgelmodul-dsp-osc.pd &
-# pd -rt -jack -channels 1 -nogui -send "id orgel07" orgelmodul-dsp-osc.pd &
-# pd -rt -jack -channels 1 -nogui -send "id orgel08" orgelmodul-dsp-osc.pd &
-# pd -rt -jack -channels 1 -nogui -send "id orgel09" orgelmodul-dsp-osc.pd &
-# pd -rt -jack -channels 1 -nogui -send "id orgel10" orgelmodul-dsp-osc.pd &
+pd -rt -jack -channels 1 -nogui -send "id orgel02" orgelmodul-dsp-osc.pd &
+pd -rt -jack -channels 1 -nogui -send "id orgel03" orgelmodul-dsp-osc.pd &
+pd -rt -jack -channels 1 -nogui -send "id orgel04" orgelmodul-dsp-osc.pd &
+pd -rt -jack -channels 1 -nogui -send "id orgel05" orgelmodul-dsp-osc.pd &
+pd -rt -jack -channels 1 -nogui -send "id orgel06" orgelmodul-dsp-osc.pd &
+pd -rt -jack -channels 1 -nogui -send "id orgel07" orgelmodul-dsp-osc.pd &
+pd -rt -jack -channels 1 -nogui -send "id orgel08" orgelmodul-dsp-osc.pd &
+pd -rt -jack -channels 1 -nogui -send "id orgel09" orgelmodul-dsp-osc.pd &
+pd -rt -jack -channels 1 -nogui -send "id orgel10" orgelmodul-dsp-osc.pd &
 # pd -rt -jack -channels 1 -send "id orgel01" orgelmodul-dsp-osc.pd &
 # pd -rt -jack -channels 1 -send "id orgel02" orgelmodul-dsp-osc.pd &
 # pd -rt -jack -channels 1 -send "id orgel03" orgelmodul-dsp-osc.pd &
