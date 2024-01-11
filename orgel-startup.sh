93419c48f83ebfa215e616217b4733daf1c010eb
ormf <junk14@selma.hfmdk-frankfurt.de>
2024-01-11 17:21:51 +0100

    start all dsp processes with gui.

--- orgel-startup.sh
+++ orgel-startup.sh
@@ -2,15 +2,15 @@
 DIR=$HOME/work/programmieren/pd/papierorgel-pd
 
 pd -rt -jack -channels 2 -send "id orgel01" $DIR/orgelmodul-dsp-osc.pd &
-pd -rt -jack -channels 2 -nogui -send "id orgel02" $DIR/orgelmodul-dsp-osc.pd &
-pd -rt -jack -channels 2 -nogui -send "id orgel03" $DIR/orgelmodul-dsp-osc.pd &
-pd -rt -jack -channels 2 -nogui -send "id orgel04" $DIR/orgelmodul-dsp-osc.pd &
-pd -rt -jack -channels 2 -nogui -send "id orgel05" $DIR/orgelmodul-dsp-osc.pd &
-pd -rt -jack -channels 2 -nogui -send "id orgel06" $DIR/orgelmodul-dsp-osc.pd &
-pd -rt -jack -channels 2 -nogui -send "id orgel07" $DIR/orgelmodul-dsp-osc.pd &
-pd -rt -jack -channels 2 -nogui -send "id orgel08" $DIR/orgelmodul-dsp-osc.pd &
-pd -rt -jack -channels 2 -nogui -send "id orgel09" $DIR/orgelmodul-dsp-osc.pd &
-pd -rt -jack -channels 2 -nogui -send "id orgel10" $DIR/orgelmodul-dsp-osc.pd &
+pd -rt -jack -channels 2 -send "id orgel02" $DIR/orgelmodul-dsp-osc.pd &
+pd -rt -jack -channels 2 -send "id orgel03" $DIR/orgelmodul-dsp-osc.pd &
+pd -rt -jack -channels 2 -send "id orgel04" $DIR/orgelmodul-dsp-osc.pd &
+pd -rt -jack -channels 2 -send "id orgel05" $DIR/orgelmodul-dsp-osc.pd &
+pd -rt -jack -channels 2 -send "id orgel06" $DIR/orgelmodul-dsp-osc.pd &
+pd -rt -jack -channels 2 -send "id orgel07" $DIR/orgelmodul-dsp-osc.pd &
+pd -rt -jack -channels 2 -send "id orgel08" $DIR/orgelmodul-dsp-osc.pd &
+pd -rt -jack -channels 2 -send "id orgel09" $DIR/orgelmodul-dsp-osc.pd &
+pd -rt -jack -channels 2 -send "id orgel10" $DIR/orgelmodul-dsp-osc.pd &
 # pd -rt -jack -channels 10 -send "id orgel01" orgelmodul-dsp-osc.pd &
 # pd -rt -jack -channels 10 -nogui -send "id orgel02" orgelmodul-dsp-osc.pd &
 # pd -rt -jack -channels 10 -nogui -send "id orgel03" orgelmodul-dsp-osc.pd &
