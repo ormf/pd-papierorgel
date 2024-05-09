373f902bff110352ae30246180b64db2bd7405cd
ormf <junk14@selma.hfmdk-frankfurt.de>
2024-05-09 13:36:05 +0200

    changed reson~ to bw control

--- orgel-startup.sh
+++ orgel-startup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 DIR=$HOME/work/programmieren/pd/papierorgel-pd
 
-pd -rt -jack -audiobuf 0 -channels 10 -send "id orgel01" $DIR/orgelmodul-dsp-osc.pd &
-pd -rt -jack -audiobuf 0 -channels 10 -send "id orgel02" $DIR/orgelmodul-dsp-osc.pd &
-# pd -rt -jack -audiobuf 0 -channels 10 -send "id orgel03" $DIR/orgelmodul-dsp-osc.pd &
-# pd -rt -jack -audiobuf 0 -channels 10 -send "id orgel04" $DIR/orgelmodul-dsp-osc.pd &
-# pd -rt -jack -audiobuf 0 -channels 10 -send "id orgel05" $DIR/orgelmodul-dsp-osc.pd &
-# pd -rt -jack -audiobuf 0 -channels 10 -send "id orgel06" $DIR/orgelmodul-dsp-osc.pd &
-# pd -rt -jack -audiobuf 0 -channels 10 -send "id orgel07" $DIR/orgelmodul-dsp-osc.pd &
-# pd -rt -jack -audiobuf 0 -channels 10 -send "id orgel08" $DIR/orgelmodul-dsp-osc.pd &
+# pd -rt -jack -audiobuf 0 -blocksize 32 -channels 10 -send "id orgel01" $DIR/orgelmodul-dsp-osc.pd &
+# pd -rt -jack -audiobuf 0 -blocksize 32 -channels 10 -send "id orgel02" $DIR/orgelmodul-dsp-osc.pd &
+# pd -rt -jack -audiobuf 0 -blocksize 32 -channels 10 -send "id orgel03" $DIR/orgelmodul-dsp-osc.pd &
+# pd -rt -jack -audiobuf 0 -blocksize 32 -channels 10 -send "id orgel04" $DIR/orgelmodul-dsp-osc.pd &
+# pd -rt -jack -audiobuf 0 -blocksize 32 -channels 10 -send "id orgel05" $DIR/orgelmodul-dsp-osc.pd &
+# pd -rt -jack -audiobuf 0 -blocksize 32 -channels 10 -send "id orgel06" $DIR/orgelmodul-dsp-osc.pd &
+# pd -rt -jack -audiobuf 0 -blocksize 32 -channels 10 -send "id orgel07" $DIR/orgelmodul-dsp-osc.pd &
+pd -rt -jack -audiobuf 0 -blocksize 32 -channels 10 -send "id orgel08" $DIR/orgelmodul-dsp-osc.pd &
 # pd -rt -jack -audiobuf 0 -channels 10 -send "id orgel09" $DIR/orgelmodul-dsp-osc.pd &
 # pd -rt -jack -audiobuf 0 -channels 10 -send "id orgel10" $DIR/orgelmodul-dsp-osc.pd &
 # pd -rt -jack -audiobuf 0 -channels 10 -send "id orgel01" orgelmodul-dsp-osc.pd &
